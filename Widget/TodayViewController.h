044e02fb3eb978484a700453a630c1106d8b9ac3
wangyong <wangyong891019@163.com>
2015-01-08 19:33:31 +0800

    蜂窝数据，widgets

---
+++ Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Widget
+//
+//  Created by wyong on 14/12/4.
+//  Copyright (c) 2014年 wyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ widget_extension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  widget_extension
+//
+//  Created by wyong on 15/1/5.
+//  Copyright (c) 2015年 wyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
